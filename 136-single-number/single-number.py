625349a3b7edd74473399f85310a96071151ae67
Muhammad Asad Ishfaq <105119180+MuhammadAsadIshfaq@users.noreply.github.com>
2024-08-18 20:10:55 +0500

    Time: 2510 ms (10.65%) | Memory: 19.6 MB (6.47%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,9 +1,13 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        l = []
-        for i in nums:
-            if i not in l:
-                if nums.count(i) == 1:
-                    return i
-        return None
+        new = set(nums)
+        for i in new:
+            if nums.count(i) == 1:
+                return i
+        # l = []
+        # for i in nums:
+        #     if i not in l:
+        #         if nums.count(i) == 1:
+        #             return i
+        # return None
         
